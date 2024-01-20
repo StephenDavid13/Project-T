0e3d7365b0f746ceda16651f9b9d0913023796de
sdavid13 <sdavid@pointhacks.com>
2024-01-21 02:57:34 +1100

    Dungeon included
    - Added quick dungeon intro
    - Added monster spawning
    - Added skeleton and goblins as monsters

---
+++ scenes/main_scenes/scripts/GoToDungeon.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.name == "main_char":
+		get_tree().change_scene_to_file("res://scenes/main_scenes/level_dungeon.tscn")
--- scenes/main_scenes/scripts/game_manager.gd
+++ scenes/main_scenes/scripts/game_manager.gd
@@ -7,11 +7,13 @@ func _ready():
 	pass
 	
 func _process(_delta):
+	#Check if player are in a sign for them to go somewhere
 	if Input.is_action_just_pressed("action_use") and isInSign :
-		if target_level == "goToBattle" :
-			get_tree().change_scene_to_file("res://scenes/main_scenes/scene_1.tscn")	
+		if target_level == "goOutside" :
+			get_tree().change_scene_to_file("res://scenes/main_scenes/outside_tower.tscn")	
 		elif target_level == "goToBase" :
 			get_tree().change_scene_to_file("res://scenes/main_scenes/main_scene_start.tscn")
+			
 
 
 func changeIsInSign(boolInSign):
